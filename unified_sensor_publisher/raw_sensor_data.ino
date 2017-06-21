65770d82e6c7a80de12f8116eeafe46f58ea85ae
Erich Deise <erichcdeise@gmail.com>
2017-06-21 00:08:51 -0400

    basic wheel encoder counter

--- unified_sensor_publisher/raw_sensor_data.ino
+++ unified_sensor_publisher/raw_sensor_data.ino
@@ -7,6 +7,7 @@
 #include <std_msgs/Float32.h>
 #include "geometry_msgs/Point.h"
 #include <std_msgs/Int8.h>
+#include <std_msgs/Int16.h>
 #include "geometry_msgs/Point.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,6 +19,14 @@ const int ir_pin = 0;
 const int TRIGGER_PIN = 10;
 const int ECHO_PIN = 12;
 long duration = 0;
+
+const int encoder_pin_right = 2;
+const int encoder_pin_left = 3;
+
+int encoderValue_R = 0;
+int encoderValue_L = 0; 
+
+//https://www.ez-robot.com/Tutorials/UserTutorials/189/1 encoder on D2 and D3
 
 //register the mag compass
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
@@ -35,6 +44,12 @@ ros::Publisher mag_data_raw("mag_data_raw", &magDataRaw);
 std_msgs::Float32 irRangeRaw;
 ros::Publisher ir_data_raw("ir_data_raw", &irRangeRaw);
 
+std_msgs::Int16 encoderRight;
+ros::Publisher encoder_data_right("encoder_data_right", &encoderRight);
+
+std_msgs::Int16 encoderLeft;
+ros::Publisher encoder_data_left("encoder_data_left", &encoderLeft);
+
 // void messageCb( const std_msgs::Empty& toggle_msg){
 //   digitalWrite(8, HIGH-digitalRead(8)); 
 //   digitalWrite(7, HIGH-digitalRead(7));   // blink the led
@@ -49,7 +64,7 @@ void ledToggleCallback( const std_msgs::Int8 &toggle_msg){
   if (strcmp(str, "1") == 0)
   {
     //nh.loginfo(str);
-    digitalWrite(7, HIGH-digitalRead(7));
+    digitalWrite(9, HIGH-digitalRead(9));
   } 
   else if (strcmp(str, "2") == 0)
   {
@@ -59,13 +74,13 @@ void ledToggleCallback( const std_msgs::Int8 &toggle_msg){
   else if (strcmp(str, "3") == 0)
   {
     //nh.loginfo(str);
-    digitalWrite(7, HIGH-digitalRead(7));   // blink the led
+    digitalWrite(9, HIGH-digitalRead(9));   // blink the led
     digitalWrite(8, HIGH-digitalRead(8));   // blink the led
   }
   else if (strcmp(str, "4") == 0) 
   {
     //nh.loginfo(str);
-    digitalWrite(7, HIGH-digitalRead(7));   // blink the led
+    digitalWrite(9, HIGH-digitalRead(9));   // blink the led
     digitalWrite(8, HIGH-digitalRead(8));   // blink the led
   } 
   else 
@@ -78,16 +93,35 @@ void ledToggleCallback( const std_msgs::Int8 &toggle_msg){
 
 ros::Subscriber<std_msgs::Int8> sub("toggle_led", &ledToggleCallback);
 
-//ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
+//ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb )
+
+void countR(){
+  encoderValue_R++;
+}
+
+void countL(){
+  encoderValue_L++;
+}
 
 void setup()
 {
-  pinMode(7, OUTPUT);
+  pinMode(9, OUTPUT);
   pinMode(8, OUTPUT);
+  pinMode(encoder_pin_right, INPUT);
+  pinMode(encoder_pin_left, INPUT);
+
+  // attachInterrupt(digitalPinToInterrupt(encoder_pin_right), countA, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(encoder_pin_left), countB, FALLING);
+  
+  attachInterrupt(digitalPinToInterrupt(encoder_pin_right), countR, FALLING);
+  attachInterrupt(digitalPinToInterrupt(encoder_pin_left), countL, FALLING);
+
   nh.initNode();
   nh.advertise(ir_data_raw);
   nh.advertise(mag_data_raw);
   nh.advertise(ultrasonic_data_raw);
+  nh.advertise(encoder_data_right);
+  nh.advertise(encoder_data_left);
   randomSeed(analogRead(0));
 
   nh.subscribe(sub);
@@ -111,6 +145,9 @@ void getRawMagnetometerData(sensors_event_t event, geometry_msgs::Point &magData
 
 
 
+
+
+
 void getRawUltraSonicData(int trigger_pin, int echo_pin, 
   std_msgs::Float32 &ultraSonicRangeData) {
 
@@ -130,8 +167,21 @@ void getRawUltraSonicData(int trigger_pin, int echo_pin,
  
 }
 
+void getEncoderDataRight(std_msgs::Int16 &encoderRight){
+
+  encoderRight.data = encoderValue_R;
+
+}
+
+void getEncoderDataLeft(std_msgs::Int16 &encoderLeft){
+
+  encoderLeft.data = encoderValue_L;
+
+}
+
 void loop()
 {
+
 
   sensors_event_t event;
   mag.getEvent(&event);
@@ -148,10 +198,14 @@ void loop()
   getRawUltraSonicData(TRIGGER_PIN, ECHO_PIN, ultraSonicRangeData);
   ultrasonic_data_raw.publish(&ultraSonicRangeData);
 
+  getEncoderDataRight(encoderRight);
+  encoder_data_right.publish(&encoderRight);
+
+  getEncoderDataLeft(encoderLeft);
+  encoder_data_left.publish(&encoderLeft);
+
 
   nh.spinOnce();
   delay(1000);
 }
-
-
 
