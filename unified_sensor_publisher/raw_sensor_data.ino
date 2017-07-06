454162f57e6e354914aea860e514f71bfad14142
Erich Deise <erichcdeise@gmail.com>
2017-07-05 23:44:13 -0400

    global memory management tweaks

--- unified_sensor_publisher/raw_sensor_data.ino
+++ unified_sensor_publisher/raw_sensor_data.ino
@@ -6,25 +6,14 @@
 #include <std_msgs/String.h>
 #include <std_msgs/Float32.h>
 #include "geometry_msgs/Point.h"
-#include <std_msgs/Int8.h>
 #include <std_msgs/Int16.h>
-#include "geometry_msgs/Point.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <std_msgs/Empty.h>
 
-#define ULTRASONIC_MAX_WAIT_COUNT 5000
-
-const int ir_pin = 0;
-const int TRIGGER_PIN = 10;
-const int ECHO_PIN = 12;
-long duration = 0;
-
-const int encoder_pin_right = 2;
-const int encoder_pin_left = 3;
-
-int encoderValue_R = 0;
-int encoderValue_L = 0; 
+int encoderValue_A = 0;
+int encoderValue_B = 0; 
+long ultrasonic_duration = 0;
 
 //https://www.ez-robot.com/Tutorials/UserTutorials/189/1 encoder on D2 and D3
 
@@ -50,71 +39,22 @@ ros::Publisher encoder_data_right("encoder_data_right", &encoderRight);
 std_msgs::Int16 encoderLeft;
 ros::Publisher encoder_data_left("encoder_data_left", &encoderLeft);
 
-// void messageCb( const std_msgs::Empty& toggle_msg){
-//   digitalWrite(8, HIGH-digitalRead(8)); 
-//   digitalWrite(7, HIGH-digitalRead(7));   // blink the led
-// }
 
-//Subscribers
-void ledToggleCallback( const std_msgs::Int8 &toggle_msg){
-
-  char str[8];
-  sprintf(str, "%d", toggle_msg.data);
-  
-  if (strcmp(str, "1") == 0)
-  {
-    //nh.loginfo(str);
-    digitalWrite(9, HIGH-digitalRead(9));
-  } 
-  else if (strcmp(str, "2") == 0)
-  {
-    //nh.loginfo(str);
-    digitalWrite(8, HIGH-digitalRead(8));
-  }
-  else if (strcmp(str, "3") == 0)
-  {
-    //nh.loginfo(str);
-    digitalWrite(9, HIGH-digitalRead(9));   // blink the led
-    digitalWrite(8, HIGH-digitalRead(8));   // blink the led
-  }
-  else if (strcmp(str, "4") == 0) 
-  {
-    //nh.loginfo(str);
-    digitalWrite(9, HIGH-digitalRead(9));   // blink the led
-    digitalWrite(8, HIGH-digitalRead(8));   // blink the led
-  } 
-  else 
-  {
-    nh.loginfo(str);
-  }
- 
+void count_encoder_A(){
+  encoderValue_A++;
 }
 
-
-ros::Subscriber<std_msgs::Int8> sub("toggle_led", &ledToggleCallback);
-
-//ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb )
-
-void countR(){
-  encoderValue_R++;
-}
-
-void countL(){
-  encoderValue_L++;
+void count_encoder_B(){
+  encoderValue_B++;
 }
 
 void setup()
 {
   pinMode(9, OUTPUT);
   pinMode(8, OUTPUT);
-  pinMode(encoder_pin_right, INPUT);
-  pinMode(encoder_pin_left, INPUT);
 
-  // attachInterrupt(digitalPinToInterrupt(encoder_pin_right), countA, FALLING);
-  // attachInterrupt(digitalPinToInterrupt(encoder_pin_left), countB, FALLING);
-  
-  attachInterrupt(digitalPinToInterrupt(encoder_pin_right), countR, FALLING);
-  attachInterrupt(digitalPinToInterrupt(encoder_pin_left), countL, FALLING);
+  attachInterrupt(digitalPinToInterrupt(2), count_encoder_A, FALLING);
+  attachInterrupt(digitalPinToInterrupt(3), count_encoder_B, FALLING);
 
   nh.initNode();
   nh.advertise(ir_data_raw);
@@ -127,11 +67,9 @@ void setup()
   nh.subscribe(sub);
 }
 
-
-
-void getIrRawData(int pin, std_msgs::Float32 &irRangeRaw)
+void getIrRawData(std_msgs::Float32 &irRangeRaw)
 {
-  irRangeRaw.data = analogRead(pin);
+  irRangeRaw.data = analogRead(0);
 }
 
 
@@ -143,51 +81,44 @@ void getRawMagnetometerData(sensors_event_t event, geometry_msgs::Point &magData
 
 }
 
+void getRawUltraSonicData(std_msgs::Float32 &ultraSonicRangeData) {
 
-
-
-
-
-void getRawUltraSonicData(int trigger_pin, int echo_pin, 
-  std_msgs::Float32 &ultraSonicRangeData) {
-
-  pinMode(trigger_pin, OUTPUT);
-  pinMode(echo_pin,INPUT);
-  digitalWrite(trigger_pin, LOW);
+  pinMode(10, OUTPUT);
+  pinMode(12,INPUT);
+  digitalWrite(10, LOW);
   delayMicroseconds(2);
-  digitalWrite(trigger_pin, HIGH);
+  digitalWrite(10, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigger_pin, LOW);
+  digitalWrite(10, LOW);
 
-  //Get duration it takes to receive echo
-  duration = pulseIn(echo_pin, HIGH);
+  //Get ultrasonic_duration it takes to receive echo
+  ultrasonic_duration = pulseIn(12, HIGH);
 
-  //Convert duration into distance
-  ultraSonicRangeData.data = duration; // /29/2;
+  //Convert ultrasonic_duration into distance
+  ultraSonicRangeData.data = ultrasonic_duration; // /29/2;
  
 }
 
 void getEncoderDataRight(std_msgs::Int16 &encoderRight){
 
-  encoderRight.data = encoderValue_R;
+  encoderRight.data = encoderValue_A;
 
 }
 
 void getEncoderDataLeft(std_msgs::Int16 &encoderLeft){
 
-  encoderLeft.data = encoderValue_L;
+  encoderLeft.data = encoderValue_B;
 
 }
 
 void loop()
 {
 
-
   sensors_event_t event;
   mag.getEvent(&event);
 
   //raw IR data
-  getIrRawData(ir_pin, irRangeRaw);
+  getIrRawData(irRangeRaw);
   ir_data_raw.publish(&irRangeRaw);
 
   //get magnetometer (compass) pose data
@@ -195,7 +126,7 @@ void loop()
   mag_data_raw.publish(&magDataRaw);
 
   //get the ultrasonic ranger 
-  getRawUltraSonicData(TRIGGER_PIN, ECHO_PIN, ultraSonicRangeData);
+  getRawUltraSonicData(ultraSonicRangeData);
   ultrasonic_data_raw.publish(&ultraSonicRangeData);
 
   getEncoderDataRight(encoderRight);
